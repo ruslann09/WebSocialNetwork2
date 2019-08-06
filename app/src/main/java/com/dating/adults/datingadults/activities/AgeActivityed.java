1f02496d7f1a8f11fa63d2cc7158aae5502bba39
Ruslan Malbrook <ruslanprogit@gmail.com>
2019-08-06 17:21:08 +0700

    init

---
+++ app/src/main/java/com/dating/adults/datingadults/activities/AgeActivityed.java
@@ -0,0 +1,35 @@
+package com.dating.adults.datingadults.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.dating.adults.datingadults.R;
+
+public class AgeActivityed extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mail);
+
+        editText = (EditText) findViewById(R.id.name_edit_text);
+        textView = (TextView) findViewById(R.id.textView2);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (editText != null && !editText.getText().toString().isEmpty()) {
+                    startActivity(new Intent(getApplicationContext(), AuthStaticActivityed.class));
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/dating/adults/datingadults/activities/Have18Activityed.java
@@ -0,0 +1,15 @@
+package com.dating.adults.datingadults.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dating.adults.datingadults.R;
+
+public class Have18Activityed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_have18);
+    }
+}
