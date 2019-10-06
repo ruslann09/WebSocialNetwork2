4ad60bf977bde31f50248151f37c2e4102e9d5c4
ruslanprogit <ruslann09@inbox.ru>
2019-10-06 13:08:01 +0700

    finished

---
+++ app/src/main/java/com/refactfriendyou/takeiteasy/meetyourdream/activities/ActivityIsAdult.java
@@ -0,0 +1,15 @@
+package com.refactfriendyou.takeiteasy.meetyourdream.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.refactfriendyou.takeiteasy.meetyourdream.R;
+
+public class ActivityIsAdult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_have18);
+    }
+}
