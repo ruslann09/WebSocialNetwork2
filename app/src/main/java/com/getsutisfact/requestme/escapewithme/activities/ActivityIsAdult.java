c103d544e027618258f178da274e202da7b63353
ruslanprogit <ruslann09@inbox.ru>
2019-10-20 16:10:23 +0700

    finished

---
+++ app/src/main/java/com/getsutisfact/requestme/escapewithme/activities/ActivityIsAdult.java
@@ -0,0 +1,15 @@
+package com.getsutisfact.requestme.escapewithme.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.getsutisfact.requestme.escapewithme.R;
+
+public class ActivityIsAdult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_have18);
+    }
+}
